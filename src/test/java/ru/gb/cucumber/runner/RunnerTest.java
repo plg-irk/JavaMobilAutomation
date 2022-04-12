e49dee493275a7e27b36bf824b0dafb0b4649732
Leonid <nk-irk@mail.ru>
2022-04-12 11:55:06 +0800

    ver.3 Cucumber 2 Scenario

--- src/test/java/ru/gb/cucumber/runner/RunnerTest.java
+++ src/test/java/ru/gb/cucumber/runner/RunnerTest.java
@@ -4,7 +4,7 @@ package ru.gb.cucumber.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = {"./src/test/java/ru/gb/cucumber/features"},
+@CucumberOptions(features = {"/src/test/java/ru/gb/cucumber/features"},
         glue = {"/src/test/java/ru/gb/cucumber/steps"})
 
 public class RunnerTest extends AbstractTestNGCucumberTests {
